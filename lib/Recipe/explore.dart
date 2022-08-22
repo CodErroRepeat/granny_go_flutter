9554d7465f96736260fe6aef549ae5a62a9afb0a
CodErroRepeat <coderrorepeat@gmail.com>
2022-08-22 18:01:39 +0530

    Reordered files

--- lib/Recipe/explore.dart
+++ lib/Recipe/explore.dart
@@ -25,7 +25,7 @@ class _ExploreState extends State<Explore> {
   }
 
   Future<void> readJson() async {
-    final String response = await rootBundle.loadString('recipes.json');
+    final String response = await rootBundle.loadString('jsonData/recipes.json');
     final List<dynamic>data = jsonDecode(response)["recipes"];
     
     setState(() {
